155bbeed9f684cb28deb9cba215ed41f03892e6b
woubuc <wouter@epicteddy.com>
2019-08-19 16:32:55 +0200

    Remove duplicate data in Metrics spec files

--- src/metrics/Metrics.Spec.ts
+++ src/metrics/Metrics.Spec.ts
@@ -1,5 +1,33 @@
 import Metrics from './Metrics';
 
+export const metricsData : any = {
+	/* eslint-disable @typescript-eslint/camelcase */
+	apdex: 0.999,
+	requests: {
+		samples: 87441,
+		failures: 2,
+		satisfied: 87357,
+		tolerated: 77,
+		by_response_time: {
+			under125: 70521,
+			under250: 71126,
+			under500: 87357,
+			under1000: 87422,
+			under2000: 87434,
+			under4000: 87438
+		}
+	},
+	timings: {
+		redirect: 0,
+		namelookup: 9,
+		connection: 88,
+		handshake: 183,
+		response: 90,
+		total: 370
+	}
+	/* eslint-enable @typescript-eslint/camelcase */
+};
+
 describe('Metrics initialisation', () => {
 
 	test('Empty data', () => {
@@ -15,33 +43,7 @@ describe('Metrics initialisation', () => {
 	});
 
 	test('Valid data', () => {
-		expect(new Metrics({
-			/* eslint-disable @typescript-eslint/camelcase */
-			apdex: 0.999,
-			requests: {
-				samples: 87441,
-				failures: 2,
-				satisfied: 87357,
-				tolerated: 77,
-				by_response_time: {
-					under125: 70521,
-					under250: 71126,
-					under500: 87357,
-					under1000: 87422,
-					under2000: 87434,
-					under4000: 87438
-				}
-			},
-			timings: {
-				redirect: 0,
-				namelookup: 9,
-				connection: 88,
-				handshake: 183,
-				response: 90,
-				total: 370
-			}
-			/* eslint-enable @typescript-eslint/camelcase */
-		})).toBeInstanceOf(Metrics);
+		expect(new Metrics(metricsData)).toBeInstanceOf(Metrics);
 	});
 
 });
